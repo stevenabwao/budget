a3db4b2bdc30febf9ec2cfc9b0d7236840d0613a
stevenabwao <stevefotieno@gmail.com>
2019-10-07 13:13:45 +0300

    changes on git ignore before commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const ExtractTextpPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextpPlugin('styles.css');
+  return  {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+       use: CSSExtract.extract({
+         use: [
+           {
+             loader: 'css-loader',
+             options: {
+               sourceMap: true
+             }
+           },
+           {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+           }
+         ]
+       })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+
+    devtool: isProduction ? 'source-map': 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  }; 
+}
