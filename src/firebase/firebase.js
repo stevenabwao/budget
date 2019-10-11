206042efd014935a63c02f0eed914a1c1ba0c404
stevenabwao <stevefotieno@gmail.com>
2019-10-11 16:42:03 +0300

    firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,36 +7,11 @@ const config = {
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGIN761009761065G_SENDER_ID,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
    
 };
 
  firebase.initializeApp(config);
 const database = firebase.database();
-
-// database.ref('expenses').on('value', (snapshot) =>{
-//     const expenses =[];
-//     snapshot.forEach((childSnapshot) =>{
-//         expenses.push({
-//          id: childSnapshot.key,
-//          ...childSnapshot.val()
-//         })
-//     });
-//     console.log(expenses)
-// })
-    
-// once uses promises
-
-// database.ref('expenses').once('value').then((snapshot) =>{
-//         const expenses =[];
-//         snapshot.forEach((childSnapshot) =>{
-//             expenses.push({
-//              id: childSnapshot.key,
-//              ...childSnapshot.val()
-//             })
-//         });
-//         console.log(expenses)
-//     })
-
 export {firebase, database as default}
--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      public: '/dist/'
+      publicPath: '/dist/'
     }
   };  
 };
